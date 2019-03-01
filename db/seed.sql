552d01947a8d9072d500b6a5b378099886057a5a
Anthony Viglione <viglione2014.2015@gmail.com>
2019-03-01 12:24:26 -0700

    added dummy data to databases and seed file. for testing

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,4 @@
+--table creation
 CREATE TABLE helo_users(
     id serial primary key,
     username varchar(20),
@@ -12,3 +13,12 @@ CREATE TABLE helo_posts(
     content text,
     author_id integer references helo_users (id)
     );
+
+-- Dummy Data first row
+INSERT INTO helo_users (username,password,profile_pic)
+VALUES ('anthony_test1', 'test1', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm4fWYG100DfpNDQJYzvlbF6eDr4zjY-QOd1GJAXqus-I5CSRt');
+SELECT * FROM helo_users;
+
+INSERT INTO helo_posts(title, img, content, author_id)
+VALUES ('first post', 'this is the first post; this is a test', 'https://upload.wikimedia.org/wikipedia/en/thumb/f/ff/SuccessKid.jpg/256px-SuccessKid.jpg',1);
+SELECT * FROM helo_posts;
